a40ce5cfd248563c0699514b564dadd8e9b9ebd6
rwanish <danatorrano@gmail.com>
2025-09-24 14:09:04 +0200

    tp1

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
-#include <Arduino.h>
 #include <WiFi.h>
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-    Serial.print("\nDefault ESP32 MAC Address: ");
-    Serial.println(WiFi.macAddress());
+  Serial.begin(115200);                // Démarre la communication série
+  delay(1000);                         // Attente pour que le moniteur série soit prêt
+ 
+  Serial.println("=== Lecture de l'adresse MAC de l'ESP32 ===");
+  String macAddress = WiFi.macAddress();  // Récupère l'adresse MAC
+  Serial.print("Adresse MAC : ");
+  Serial.println(macAddress);            // L'affiche dans le moniteur série
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Rien à faire ici pour ce TP
 }
+
 
